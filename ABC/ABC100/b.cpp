6fc2cbda6dd3718e682615875e54b59ee060d58f
kinaphar <mi8kina@gmail.com>
2023-08-09 14:14:59 +0900

    ABC100 B

---
+++ ABC/ABC100/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/modint>
+using namespace std;
+typedef long long ll;
+using Graph = vector<vector<int>>;
+#define I_MAX 2147483647;
+#define LL_MAX 9223372036854775806;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+
+int main() {
+  int d, n;
+  cin >> d >> n;
+
+  if (n == 100) {
+    cout << "101";
+  } else {
+    cout << n;
+  }
+  rep(i, d) { cout << "00"; }
+
+  cout << endl;
+}
